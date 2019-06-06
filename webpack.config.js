1570d69cb908f6211d24d2bc7423d96cc6147110
Mayank Gureja <mayank.gureja@gmail.com>
2019-06-07 02:42:15 +0530

    Cleaned up Webpack: assets are now injected instead of having to add the static code; favicons are now injected

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -10,7 +12,7 @@ module.exports = {
   entry: ['./src/js/app.js'],
   devServer: {
     watchContentBase: true,
-    contentBase: path.resolve(__dirname + 'src')
+    contentBase: path.resolve(__dirname, 'src')
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -34,8 +36,10 @@ module.exports = {
         use: {
           loader: 'url-loader',
           options: {
-            name: 'img/[name].[ext]',
-            limit: 8000 // Convert images < 8kb to base64 strings
+            name: '[name].[ext]',
+            outputPath: '../img/',
+            limit: 10000, // Convert images < 10kb to base64 strings
+            emitFile: false // Don't emit file since this is taken care of by CopyWebpackPlugin
           }
         }
       },
@@ -47,10 +51,35 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
     new MiniCssExtractPlugin({ filename: 'css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'src/img/', to: 'img' }])
-  ]
+    new CopyWebpackPlugin([{ from: 'src/img/', to: 'img' }]),
+    new FaviconsWebpackPlugin({
+      logo: './src/img/favicon.png',
+      prefix: './img/favicons/',
+      inject: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: false,
+        favicons: true,
+        firefox: true,
+        windows: true
+      }
+    })
+  ].concat(
+    // This function generates HTML files for each .html in /src directory
+    fs
+      .readdirSync('./src')
+      .filter(fn => fn.endsWith('.html'))
+      .map(html_file => {
+        // Split names and extensions
+        const parts = html_file.split('.');
+        const filename = parts[0];
+        // Create new HtmlWebpackPlugin with options
+        return new HtmlWebpackPlugin({
+          filename: `${filename}.html`,
+          template: `./src/${filename}.html`
+        });
+      })
+  )
 };
